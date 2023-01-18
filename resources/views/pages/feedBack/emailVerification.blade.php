5d2197b491560703c8d0f7ddba7c9aaf107ca04b
fatkhul jabal <mifjak1@gmail.com>
2023-01-19 00:02:41 +0700

    file

--- resources/views/pages/feedBack/emailVerification.blade.php
+++ resources/views/pages/feedBack/emailVerification.blade.php
@@ -20,51 +20,41 @@
                     <div class="mt-5 text-weight-700 fs-44" style="font-weight: bold;">Email anda gagal diverifikasi!</div>
                     <div class="mt-5 text-tertiary text-weight-400 fs-20">Tautan yang Anda gunakan sudah kadaluarsa (Expired), silahkan kirim ulang tautan yang baru!</div>
                 </div>
-                <a href="{{ url('/package/umroh') }}" class="btn mt-5 text-green" style="border: 1px solid #006B67;">
+
+                <button class="btn mt-5 text-green" style="border: 1px solid #006B67;" data-bs-toggle="modal" data-bs-target="#modal-alert">
                     <img src="{{ asset('assets-web/img/icon/icon-bolak-balik.png') }}" alt="{{ asset('assets-web/img/icon/icon-bolak-balik.png') }}"> Kirim ulang tautan
-                </a>
+                </button>
             </div>
         </div>
     @endif
     <!-- Banner End -->
 
     <!-- modal -->
-    <!--begin::Alert-->
-    <div class="alert alert-dismissible bg-light-danger d-flex flex-center flex-column py-10 px-10 px-lg-20 mb-10">
-        <!--begin::Close-->
-        <button type="button" class="position-absolute top-0 end-0 m-2 btn btn-icon btn-icon-danger" data-bs-dismiss="alert">
-            <span class="svg-icon svg-icon-1">...</span>
-        </button>
-        <!--end::Close-->
+    <div class="modal fade" tabindex="-1" id="modal-alert">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title">Modal title</h3>
 
-        <!--begin::Icon-->
-        <span class="svg-icon svg-icon-5tx svg-icon-danger mb-5">...</span>
-        <!--end::Icon-->
+                    <!--begin::Close-->
+                    <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
+                        <span class="svg-icon svg-icon-1"></span>
+                    </div>
+                    <!--end::Close-->
+                </div>
 
-        <!--begin::Wrapper-->
-        <div class="text-center">
-            <!--begin::Title-->
-            <h1 class="fw-bold mb-5">This is an alert</h1>
-            <!--end::Title-->
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
 
-            <!--begin::Separator-->
-            <div class="separator separator-dashed border-danger opacity-25 mb-5"></div>
-            <!--end::Separator-->
-
-            <!--begin::Content-->
-            <div class="mb-9 text-dark">
-                The alert component can be used to highlight certain parts of your page for <strong>higher content visibility</strong>.<br/>
-                Please read our <a href="#" class="fw-bold me-1">Terms and Conditions</a> for more info.
-            </div>
-            <!--end::Content-->
-
-            <!--begin::Buttons-->
-            <div class="d-flex flex-center flex-wrap">
-                <a href="#" class="btn btn-outline btn-outline-danger btn-active-danger m-2">Cancel</a>
-                <a href="#" class="btn btn-danger m-2">Ok, I got it</a>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
             </div>
         </div>
     </div>
+
 @endsection
 
 
