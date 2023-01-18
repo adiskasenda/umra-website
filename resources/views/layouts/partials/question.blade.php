f8f9c25e9be012450c3224e5e2c5a0ceeb3f5801
fatkhul jabal <mifjak1@gmail.com>
2023-01-18 18:40:00 +0700

    fixing css landing

--- resources/views/layouts/partials/question.blade.php
+++ resources/views/layouts/partials/question.blade.php
@@ -1,14 +1,14 @@
 <div class="container-fluid py-5 bg-image-1">
     <div class="container mt-20 mb-20">
         <div class="row">
-            <div class="col-md-8 mt-2 mb-2">
+            <div class="col-md-8 mt-2 mb-3">
                 <div class="text-white ms-5">
                     <div class="text-weight-600 fs-32 mb-5" style="font-weight: bold;">Punya pertanyaan seputar paket perjalanan?</div>
                     <div class="text-weight-400 fs-16">Customer service kami siap membantu anda terkait paket perjalanan sesuai kebutuhan anda.</div>
                 </div>
             </div>
-            <div class="col-md-4 mt-2 mb-2">
-                <a href="https://zeals.asia/" class="btn mb-4" style="color:#7CB041; background-color:white; width: 100%; border-color:white;">Daftar Affiliate</a>
+            <div class="col-md-4 mt-2 mb-3">
+                <a href="https://zeals.asia/" class="btn mb-4 mt-4" style="color:#7CB041; background-color:white; width: 100%; border-color:white;">Daftar Affiliate</a>
                 <div class="text-center mx-auto">
                     <a href="#" class="text-white">Syarat & Ketentuan</a>
                 </div>
