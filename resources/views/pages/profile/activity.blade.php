3959e66a65875e5d4efced87b1be62aff3f74195
fatkhul jabal <mifjak1@gmail.com>
2023-01-21 23:31:30 +0700

    update layout profile

---
+++ resources/views/pages/profile/activity.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container-fluid py-5 mb-8">
+        <div class="container">
+            @include('pages.transaction.partials.breadcrumb',[
+                'breadcrumb' => 'hello'
+            ])
+            <div class="row">
+                <div class="col-md-3">
+                    @include('pages.profile.partials.sidebar')
+                </div>
+                <div class="col-md-9">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="font-normal-700 fs-20 text-left mt-3"> Aktivitas Anda</div>
+                        </div>
+                        <div class="col-6 text-right">
+                            <div class="row">
+                                <div class="col-8"></div>
+                                <div class="col-4">
+                                    <select class="form-select" aria-label="Select example">
+                                        <option>Terbaru</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-responsive mt-5">
+                        <table class="table table-rounded table-flush">
+                            <tbody>
+                                <tr class="py-5 fw-semibold  border-bottom border-gray-300 fs-6">
+                                    <td>
+                                        <div class="font-normal-700 fs-14"><i class="fa-solid fa-circle-check" style="margin-right: 10px; color:green;"></i>Anda berhasil login!</div>
+                                        <div class="font-normal-500 fs-12 px-7">Anda berhasil login pada perangkat android pada 12 feb 2023 19:31</div>
+                                    </td>
+                                    <td class="font-normal-500 fs-12 text-right">2 menit</td>
+                                </tr>
+                                <tr class="py-5 fw-semibold  border-bottom border-gray-300 fs-6">
+                                    <td>
+                                        <div class="font-normal-700 fs-14"><i class="fa-solid fa-circle-check" style="margin-right: 10px; color:green;"></i>Anda berhasil login!</div>
+                                        <div class="font-normal-500 fs-12 px-7">Anda berhasil login pada perangkat android pada 12 feb 2023 19:31</div>
+                                    </td>
+                                    <td class="font-normal-500 fs-12 text-right">2 menit</td>
+                                </tr>
+                                <tr class="py-5 fw-semibold  border-bottom border-gray-300 fs-6">
+                                    <td>
+                                        <div class="font-normal-700 fs-14"><i class="fa-solid fa-circle-check" style="margin-right: 10px; color:green;"></i>Anda berhasil login!</div>
+                                        <div class="font-normal-500 fs-12 px-7">Anda berhasil login pada perangkat android pada 12 feb 2023 19:31</div>
+                                    </td>
+                                    <td class="font-normal-500 fs-12 text-right">2 menit</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/pages/profile/profile.blade.php
+++ resources/views/pages/profile/profile.blade.php
@@ -11,11 +11,52 @@
                     @include('pages.profile.partials.sidebar')
                 </div>
                 <div class="col-md-9">
-                    <div> Profile </div>
-                    <div class="card card-bordered" style="width: 100%;">
-                        <div>Foto Profile</div>
-                        <div>Information Pemilik Akun</div>
-                        <div>Email dan Nomor Ponsel</div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="font-normal-700 fs-20"> Profil</div>
+                        </div>
+                        <div class="col-6">
+                            <div class="font-normal-500 fs-16 text-right">
+                                <a href="{{ url('/profile/edit') }}" class="text-green">
+                                    <i class="fa-solid fa-pen-to-square" style="color: green"></i>
+                                    Edit Profil
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card card-bordered mt-5" style="width: 100%;">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="font-normal-400 fs-14">Nama Lengkap</div>
+                                    <div class="font-normal-700 fs-16 mt-1">Reonolgy</div>
+                                </div>
+                                <div class="col-7">
+                                    <div class="font-normal-400 fs-14">Alamat Domisili</div>
+                                    <div class="font-normal-700 fs-16 mt-1">Jakarta</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="font-normal-400 fs-14 mt-10">Jenis Kelamin</div>
+                                    <div class="font-normal-700 fs-16 mt-1">Laki-laki <span><i class="fa-solid fa-mars" style="color: black"></i></span></div>
+                                </div>
+                                <div class="col-7">
+                                    <div class="font-normal-400 fs-14 mt-10">Email</div>
+                                    <div class="font-normal-700 fs-16 mt-1">Reonolgy@gmail.com</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="font-normal-400 fs-14 mt-10">Tanggal Lahir</div>
+                                    <div class="font-normal-700 fs-16 mt-1">15 Januari 1977</div>
+                                </div>
+                                <div class="col-7">
+                                    <div class="font-normal-400 fs-14 mt-10">Nomor Telepon</div>
+                                    <div class="font-normal-700 fs-16 mt-1">0872356123521</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
